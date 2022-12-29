a8ce74ed3bd36204ed29f7e98a0cabae90ab8153
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2022-12-29 04:14:40 +0000

    Work section

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,3 @@
-// const coffee = require( 'coffee-machine' );
-// let today = new Date();
-// let hours = today.getHours();
-
-
-// class Coffee extends Energy {
-//   constructor(code) {
-//     let code = [];
-//     const tired = sleepyEyes(e);
-
-//     if (tired === true 
-//         && (hours >= 09 && hours < 22)) {
-//       code = tired.coffee.get().code.push();
-//     } else {
-//       code = tired.bed.sleep(); 
-//     }
-
-//     return code[0];
-//   }
-// }
-
 window.addEventListener('load', function () {
   const code = document.getElementById('hero-code-block');
   code.parentNode.classList.remove('done');
