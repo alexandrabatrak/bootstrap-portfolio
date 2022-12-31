0c4093ac128a45f279f65a1367d5b7748e366ce4
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2022-12-31 06:24:40 +0000

    Update JS, add onscroll animation

--- dist/index.js
+++ dist/index.js
@@ -1,74 +1,100 @@
-window.addEventListener("load", function () {
-  // Don't animate if user prefers reduced motion
-  // Credit  https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9#:~:text=You%20can%20still%20disable%20those,%2Dmotion%3A%20reduce)%60).
-  const reducedMotion =
-    window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
-    window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+(function () {
+  'use strict';
 
-  // if (!!reducedMotion) {
-  //   return;
-  // } else {
-  // }
+  window.addEventListener('load', function () {
+    let loader = document.getElementById('#loader');
+    if (loader) {
+      loader.classList.add('d-none');
+    }
 
-  if (reducedMotion === false) {
-    const typedText = document.getElementById("typed-text");
-    let str = typedText.innerHTML;
-    let i = 0;
-    typedText.innerHTML = "";
+    // Don't animate if user prefers reduced motion
+    // Credit to Nathan @ https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9#:~:text=You%20can%20still%20disable%20those,%2Dmotion%3A%20reduce)%60).
+    const reducedMotion =
+      window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
+      window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
 
-    let typing = setInterval(function () {
-      i++;
-      typedText.innerHTML = str.slice(0, i) + "|";
-      if (i == str.length) {
-        clearInterval(typing);
-        typedText.innerHTML = str;
+    if (reducedMotion === false) {
+      const typedText = document.getElementById('typed-text');
+      typedText.parentNode.style.opacity = '0';
+      let str = typedText.innerHTML;
+      let i = 0;
+      typedText.innerHTML = '';
+
+      let typing = setInterval(function () {
+        i++;
+        typedText.innerHTML = str.slice(0, i) + '|';
+        typedText.parentNode.style.opacity = '1';
+        if (i == str.length) {
+          clearInterval(typing);
+          typedText.innerHTML = str;
+        }
+      }, 1);
+    } else {
+      const video = document.getElementById('#videoBg');
+      video.pause();
+      // return;
+    }
+
+    // NAV scroll behavior
+    const nav = document.querySelector('nav');
+    const startchange = document
+      .querySelector('.title-wrapper')
+      .getBoundingClientRect();
+    let didScroll = false;
+    // let lastScrollTop = 0;
+    // const delta = 5;
+    // const navbarHeight = nav.getBoundingClientRect().height;
+    var offset = startchange.top + window.scrollY;
+
+    window.addEventListener('scroll', function () {
+      didScroll = true;
+    });
+
+    setInterval(function () {
+      if (didScroll) {
+        let st = window.scrollY;
+        if (st > offset) {
+          nav.classList.add('bg-black');
+          nav.classList.remove('bg-transparent');
+        } else {
+          nav.classList.remove('bg-black');
+          nav.classList.add('bg-transparent');
+        }
+        didScroll = false;
       }
-    }, 1);
-  } else {
-    return;
-  }
-
-  // NAV scroll behavior
-  const nav = document.querySelector("nav");
-  const startchange = document.querySelector("main").getBoundingClientRect();
-  let didScroll = false;
-  let lastScrollTop = 0;
-  const delta = 5;
-  const navbarHeight = nav.getBoundingClientRect().height;
-  var offset = startchange.top + window.scrollY;
-
-  window.addEventListener("scroll", function () {
-    didScroll = true;
+    }, 250);
   });
 
-  setInterval(function () {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, 250);
+  // On scroll animation
+  // Credit to Shiho Kazama @ https://dev.to/miacan2021/fade-in-animation-on-scroll-with-intersectionobserver-vanilla-js-4p27
+  // const reveal = document.querySelector('.reveal');
+  // const callback = function (entries) {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       entry.target.classList.add('inview');
+  //     } else {
+  //       entry.target.classList.remove('inview');
+  //     }
+  //   });
+  // };
+  // const io = new IntersectionObserver(callback);
+  // io.observe(reveal);
 
-  function hasScrolled() {
-    let st = window.scrollY;
-    if (st > offset) {
-      nav.classList.add("bg-black");
-      nav.classList.remove("bg-transparent");
-    } else {
-      nav.classList.remove("bg-black");
-      nav.classList.add("bg-transparent");
-    }
+  const items = document.querySelectorAll('.reveal');
 
-    // move nav up when scrolled down
-
-    // if (Math.abs(lastScrollTop - st) <= delta) return;
-
-    // if (st > lastScrollTop && st > navbarHeight) {
-    //   nav.classList.add("is-up");
-    // } else {
-    //   if (st - window.innerHeight) {
-    //     nav.classList.remove("is-up");
-    //   }
-    // }
-    // lastScrollTop = st;
+  const active = function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('inview');
+      } else {
+        entry.target.classList.remove('inview');
+      }
+    });
+  };
+  const io2 = new IntersectionObserver(active);
+  for (let i = 0; i < items.length; i++) {
+    io2.observe(items[i]);
   }
-});
+
+  const pure = new PureCounter();
+})();
