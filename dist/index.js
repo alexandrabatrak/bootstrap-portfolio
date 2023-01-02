8d59edc1935fe194e2475b79a02a28ca08a7a307
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-02 06:45:29 +0000

    Update JS

--- dist/index.js
+++ dist/index.js
@@ -1,100 +1,175 @@
 (function () {
   'use strict';
 
-  window.addEventListener('load', function () {
-    let loader = document.getElementById('#loader');
+  // Loader
+  window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('#loader');
     if (loader) {
-      loader.classList.add('d-none');
+      setInterval(() => {
+        // loader.classList.add('d-none');
+        loader.remove();
+      }, 500);
     }
 
-    // Don't animate if user prefers reduced motion
-    // Credit to Nathan @ https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9#:~:text=You%20can%20still%20disable%20those,%2Dmotion%3A%20reduce)%60).
-    const reducedMotion =
-      window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
-      window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+    // Don't animate or play video if user prefers reduced motion
+    const reducedMotion = window.matchMedia(
+      '(prefers-reduced-motion: reduce)'
+    ).matches;
+    if (reducedMotion) {
+      videoPause();
+    } else {
+      typedText();
+    }
 
-    if (reducedMotion === false) {
+    // Typing animation
+    // Using requestAnimationFrame is better for perfomance than using setInterval
+    function typedText() {
       const typedText = document.getElementById('typed-text');
       typedText.parentNode.style.opacity = '0';
       let str = typedText.innerHTML;
       let i = 0;
       typedText.innerHTML = '';
 
-      let typing = setInterval(function () {
-        i++;
+      function typing() {
+        i += 3; // Increase the value of i by 3 in each iteration
         typedText.innerHTML = str.slice(0, i) + '|';
         typedText.parentNode.style.opacity = '1';
         if (i == str.length) {
-          clearInterval(typing);
+          //~AnimationFrame is better for perfomance than setInterval
+          cancelAnimationFrame(typing);
           typedText.innerHTML = str;
+        } else {
+          requestAnimationFrame(typing);
         }
-      }, 1);
-    } else {
-      const video = document.getElementById('#videoBg');
-      video.pause();
-      // return;
+      }
+
+      requestAnimationFrame(typing);
+      // To execute animation only once
+      cancelAnimationFrame(typing);
     }
+
+    // setInterval version of typedText
+    // function typedText() {
+    //  const typedText = document.getElementById('typed-text');
+    //  typedText.parentNode.style.opacity = '0';
+    //  let str = typedText.innerHTML;
+    //  let i = 0;
+    //  typedText.innerHTML = '';
+
+    //  let typing = setInterval(() => {
+    //    i++;
+    //    typedText.innerHTML = str.slice(0, i) + '|';
+    //    typedText.parentNode.style.opacity = '1';
+    //    if (i == str.length) {
+    //      clearInterval(typing);
+    //      typedText.innerHTML = str;
+    //    }
+    //  }, 1);
+    //}
+
+    // Video pause
+    const video = document.getElementById('videoBg');
+    const btn = document.getElementById('pauseBtn');
+    function videoPause() {
+      video.pause();
+      btn.classList.remove('fa-pause');
+      btn.classList.add('fa-play');
+      btn.firstElementChild.innerHTML = 'Pause';
+    }
+
+    btn.addEventListener('click', () => {
+      if (video.paused) {
+        video.play();
+        btn.classList.remove('fa-play');
+        btn.classList.add('fa-pause');
+        btn.firstElementChild.innerHTML = 'Play';
+      } else {
+        videoPause();
+      }
+    });
 
     // NAV scroll behavior
     const nav = document.querySelector('nav');
     const startchange = document
       .querySelector('.title-wrapper')
       .getBoundingClientRect();
-    let didScroll = false;
-    // let lastScrollTop = 0;
-    // const delta = 5;
-    // const navbarHeight = nav.getBoundingClientRect().height;
     var offset = startchange.top + window.scrollY;
 
-    window.addEventListener('scroll', function () {
-      didScroll = true;
-    });
-
-    setInterval(function () {
-      if (didScroll) {
-        let st = window.scrollY;
-        if (st > offset) {
-          nav.classList.add('bg-black');
-          nav.classList.remove('bg-transparent');
-        } else {
-          nav.classList.remove('bg-black');
-          nav.classList.add('bg-transparent');
-        }
-        didScroll = false;
-      }
-    }, 250);
-  });
-
-  // On scroll animation
-  // Credit to Shiho Kazama @ https://dev.to/miacan2021/fade-in-animation-on-scroll-with-intersectionobserver-vanilla-js-4p27
-  // const reveal = document.querySelector('.reveal');
-  // const callback = function (entries) {
-  //   entries.forEach((entry) => {
-  //     if (entry.isIntersecting) {
-  //       entry.target.classList.add('inview');
-  //     } else {
-  //       entry.target.classList.remove('inview');
-  //     }
-  //   });
-  // };
-  // const io = new IntersectionObserver(callback);
-  // io.observe(reveal);
-
-  const items = document.querySelectorAll('.reveal');
-
-  const active = function (entries) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('inview');
+    window.addEventListener('scroll', () => {
+      const st = window.scrollY;
+      if (st > offset) {
+        nav.classList.add('bg-black');
+        nav.classList.remove('bg-transparent');
       } else {
-        entry.target.classList.remove('inview');
+        nav.classList.remove('bg-black');
+        nav.classList.add('bg-transparent');
       }
     });
-  };
-  const io2 = new IntersectionObserver(active);
-  for (let i = 0; i < items.length; i++) {
-    io2.observe(items[i]);
-  }
 
-  const pure = new PureCounter();
+    // On scroll animation
+    // Source @ https://dev.to/miacan2021/fade-in-animation-on-scroll-with-intersectionobserver-vanilla-js-4p27
+    const items = document.querySelectorAll('.reveal');
+
+    // to keep applying class everytime scrolled into element
+    // const show = (entries) => {
+    //   entries.forEach((entry) => {
+    //     if (entry.isIntersecting) {
+    //       entry.target.classList.add('inview');
+    //     } else {
+    //       entry.target.classList.remove('inview');
+    //     }
+    //   });
+    // };
+
+    // to apply only once
+    const show = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('inview');
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(show, {
+      once: true,
+    });
+    items.forEach((item) => observer.observe(item));
+
+    // Reverse order of projects
+    const container = document.querySelector('.work .grid');
+    const child = Array.from(document.getElementsByClassName('project'));
+    child.reverse().forEach((div) => container.appendChild(div));
+
+    // Form validation by Bootstrap
+    const formValidation = () => {
+      const forms = document.querySelectorAll('.needs-validation');
+
+      // Loop over them and prevent submission
+      Array.from(forms).forEach((form) => {
+        form.addEventListener('submit', (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        });
+      });
+    };
+    formValidation();
+
+    // Swiper init
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    // Purecounter init
+    const pure = new PureCounter();
+  });
 })();
